a63f712f89b943360272b628c7e6adf853a88151
eyramtay <71682389+eyramtay@users.noreply.github.com>
2021-05-01 09:14:10 -0500

    added new images

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,5 +1,10 @@
 const galleryItems = [
-    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 }
+    { id: 1, path: 'images/goat_small.jpg', description: 'Photo of a goat taken at Glacier National Park.', likes: 0 },
+    { id: 2, path: 'images/finley.jpg', description: 'Photo of my dog, Finley.', likes: 0},
+    { id: 3, path: 'images/parents.jpg', description: 'Photo of my parents at my mom\'s 60th birthday party.', likes: 0 },
+    { id: 4, path: 'images/suga.jpeg', description: 'Best friend & love of my life.', likes: 0},
+    { id: 5, path: 'images/edwards2.jpeg', description: 'Life motto.', likes: 0},
+    { id: 6, path: 'images/colita.jpeg', description: 'Tacos are life.', likes: 0}
 ];
 
 module.exports = galleryItems;
