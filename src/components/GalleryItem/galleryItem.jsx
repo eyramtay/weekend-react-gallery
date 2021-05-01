90173a184b30db09df7d549ede8b9f2a959e5522
eyramtay <71682389+eyramtay@users.noreply.github.com>
2021-05-01 16:14:01 -0500

    added function to count number of likes

--- src/components/GalleryItem/galleryItem.jsx
+++ src/components/GalleryItem/galleryItem.jsx
@@ -2,3 +2,43 @@ import { useState } from 'react';
 import axios from 'axios';
 import './GalleryItem.css';
 
+function GalleryItem (props) {
+    // places imageData from props into a variable
+    let imageData = props.imageData;
+
+    // Hook to manage conditional on whether image or description is rendered
+    const [click, setClick] = useState(true);
+   
+    // Function used to toggle between picture & description
+    const toggle = () => setClick(!click);
+
+    // Function to record number of clicks for like button
+    const likeCounter = () => {
+        console.log('Click', imageData.id);
+        axios({
+            method: 'PUT',
+            url: '/gallery/like/${imageData.id}',
+        })
+        .then((response) => {
+            console.log('Increased likes for image', response);
+        })
+        .catch((error) => {
+            console.log('Error increasing likes');
+            alert('Error increasing likes');
+        });
+    };
+
+    return (
+        <>
+        {/* div to load images or description */}
+        <div>
+            {/* Click hook to determine whether image or description should be rendered */}
+            {click ? (
+            ) : (
+                <p>{imageData.description}</p>
+            )
+            )}
+        </div>
+    )
+
+}
