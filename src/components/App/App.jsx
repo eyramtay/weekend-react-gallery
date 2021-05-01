c3a45ec9443495f327fe566146aee68d0e1717cd
eyramtay <71682389+eyramtay@users.noreply.github.com>
2021-05-01 10:01:02 -0500

    added getPicture function w/ axios

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,32 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+import GalleryItem from '../GalleryItem/galleryItem';
+import GalleryList from '../GalleryList/galleryList';
 
 function App() {
+
+  const [pictureArray, setPictureArray] = useState();
+
+  useEffect(() => {
+    getPicture();
+  }, []);
+
+  function getPicture() {
+    axios({
+      method: 'GET',
+      url: '/gallery',
+    }).then(response => {
+      setPictureArray(response.data);
+      console.log('Got pictures from server', response);
+    }).catch((error) => {
+      console.log('Unable to get pictures from server', error);
+      alert('Unable to get pictures from server. Try again.');
+    });
+    return (pictureArray)
+  }
     return (
       <div className="App">
         <header className="App-header">
