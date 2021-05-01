5255e705425b3d04036ece95655b13fdd8bf07da
eyramtay <71682389+eyramtay@users.noreply.github.com>
2021-05-01 15:43:49 -0500

    added imports to GalleryItem

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -27,15 +27,27 @@ function App() {
     });
     return (pictureArray)
   }
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Gallery of My Life</h1>
         </header>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
-      </div>
-    );
+
+        {// prevents GalleryList from running until picture data is received from the server
+        pictureArray ?
+        // GalleryList sets up page elements where pictures will be displayed
+        <GalleryList pictureArray={pictureArray, getPicture()} />
+        :
+          // Empty div to render until pictureArray is filled
+          <div></div>
+        }
+        </div>
+      );
+      //   <p>Gallery goes here</p>
+      //   <img src="images/goat_small.jpg"/>
+      // </div>
+    
 }
 
 export default App;
